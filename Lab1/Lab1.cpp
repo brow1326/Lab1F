328b46cab98f98dcbd06fcfb314f214f12fcb75b
Harry <brow1326@algonquinlive.com>
2022-05-29 19:11:27 -0400

    Finished constructors

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,46 @@
+﻿// Lab1.cpp : Defines the entry point for the application.
+//
+
+#include "Lab1.h"
+#include <iostream>
+
+using namespace std;
+
+namespace CST8219
+{
+	class Vehicle
+	{
+	private:
+		int numWheels;
+		int numDoors;
+
+	public:
+		Vehicle(int w, int d)
+		{
+			numWheels = w;
+			numDoors = d;
+		}
+
+		Vehicle(int w)
+		{
+			Vehicle(w, 4);
+		}
+
+		Vehicle()
+		{
+			Vehicle(4);
+		}
+	};
+}
+
+
+
+
+
+
+
+int main(int argc, char** argv)
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
