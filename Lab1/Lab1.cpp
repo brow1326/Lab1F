0e46bbc69f62c79cd513c304fc0296ed6760c739
Harry <brow1326@algonquinlive.com>
2022-05-29 19:36:45 -0400

    Finished destructor

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -19,16 +19,24 @@ namespace CST8219
 		{
 			numWheels = w;
 			numDoors = d;
+			cout << "In constructor with 2 parameters" << endl;
 		}
 
 		Vehicle(int w)
 		{
 			Vehicle(w, 4);
+			cout << "In constructor with 1 parameters, wheels = " << w << endl;
 		}
 
 		Vehicle()
 		{
 			Vehicle(4);
+			cout << "In constructor with 0 parameters" << endl;
+		}
+
+		Vehicle::~Vehicle() 
+		{ 
+			cout << "In destructor" << endl;
 		}
 	};
 }
